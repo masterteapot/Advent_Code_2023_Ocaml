52397790508c020a578d6844cd8a9a15d3bf0d4e
jared <jared.s.kane@gmail.com>
2023-12-23 00:53:59 +0400

    Part 1 maybe done?

--- lib/day_12.ml
+++ lib/day_12.ml
@@ -1,5 +1,5 @@
 open Batteries
-(* open Utilities *)
+open Utilities
 
 let print_diagram map =
   let field, digits =
@@ -7,12 +7,7 @@ let print_diagram map =
     | field, digits -> field, digits
   in
   print_string "| [ ";
-  List.iter
-    (fun x ->
-      print_string "[ ";
-      List.iter (fun y -> Printf.printf "%c " y) x;
-      print_string "] ")
-    field;
+  List.iter (fun x -> Printf.printf "%c " x) field;
   print_string "] ";
   print_string " ||  ( ";
   List.iter (fun x -> Printf.printf "%d " x) digits;
@@ -25,7 +20,7 @@ let parse_inputs input =
     | [] -> acc
     | hd :: tl ->
       let groups = String.split_on_char ' ' hd in
-      let left = List.hd groups |> String.split_on_char '.' |> List.filter (fun x -> x <> "") in
+      let left = List.hd groups |> String.explode in
       let right =
         List.at groups 1 |> String.split_on_char ',' |> List.map int_of_string
       in
@@ -34,24 +29,29 @@ let parse_inputs input =
   aux input []
 ;;
 
-let rec score map key =
-    match map, key with
-    | _, [] -> 1
-    | [], _ -> 0
-    | a :: atl, b :: btl when a = b -> score atl btl
-    | _ :: atl, bls -> score atl bls
+(* TODO need to remove 0s and need to add acc when not 0s into if/else statement *)
+let walker (map, key) =
+  let key_length = List.length key in
+  let rec aux counter acc = function
+    | '#' :: tl -> aux (succ counter) acc tl
+    | '.' :: tl when counter > 0 -> aux 0 (counter :: acc) tl
+    | '.' :: tl -> aux 0 acc tl
+    | '?' :: tl when counter > 0 -> aux (succ counter) acc tl + aux 0 (counter :: acc) tl
+    | '?' :: tl -> aux (succ counter) acc tl + aux 0 acc tl
+    | [] ->
+      let acc = if counter > 0 then counter :: acc else acc in
+      if List.length acc <> key_length
+      then 0
+      else (
+        let score = if (List.rev acc) = key then 1 else 0 in
+        (* print_endline "we scored the group :"; *)
+        (* List.iter print_int (List.rev (counter :: acc)); *)
+        (* Printf.printf " as: %d\n\n" score; *)
+        score)
+    | _ -> failwith "unexpected character in map"
+  in
+  aux 0 [] map
 ;;
-
-
-let t = score [2; 1; 3; 2; 3] [1; 2; 3] 
-
-let () = print_newline ()
-let () = print_newline ()
-
-let () = print_int t
-
-let () = print_newline ()
-let () = print_newline ()
 
 (* ???.### 1,1,3 <-- 1 *)
 (* .??..??...?##. 1,1,3 <-- 4 *)
@@ -62,9 +62,23 @@ let () = print_newline ()
 
 (* Total is 21 arrangements *)
 
+let () = print_newline ()
+let input = read_lines "./inputs/12_t.txt" |> remove_empty_string |> parse_inputs
+
+let counted = List.map walker input
+let res_1 = (List.fold_left ( + ) 0 counted)
+let t = List.at input 5
+let tv = walker t
+
+(* let () = List.iter (fun x -> Printf.printf " %d " x) counted *)
+let () = print_newline ()
+let () = print_diagram t
+let () = print_newline ()
+let () = print_newline ()
+
 (* Part 1 *)
 let part_one () =
-  let out_1 = 1 in
+  let out_1 = res_1 in
   Printf.printf "Day 12 Part 1 --> %d\n" out_1
 ;;
 
