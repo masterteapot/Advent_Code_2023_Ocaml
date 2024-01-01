bb9db4cfb61c3f5ecc4f23db1a9ce02f7ddcd320
masterteapot <71963205+masterteapot@users.noreply.github.com>
2024-01-01 19:04:13 +0400

    Day 17 still running, I'm sure there is a faster way of doing this

--- lib/day_17.ml
+++ lib/day_17.ml
@@ -40,39 +40,61 @@ let get_directions x y d m =
   | W, Straight -> pred x, y, W
 ;;
 
-let walker aai =
+let calc_distance start finish =
+  let x_dist = Int.abs @@ (fst start - fst finish) in
+  let y_dist = Int.abs @@ (snd start - snd finish) in
+  x_dist + y_dist
+;;
+
+let rec update_ordered_list x ls acc =
+  match ls with
+  | [] -> List.rev (x :: acc)
+  | hd :: tl when x < hd -> update_ordered_list x tl (hd :: acc)
+  | hd :: tl when x >= hd -> List.rev (x :: acc) @ (hd :: tl)
+  | _ -> failwith "The logic for reverse lists is wrong"
+;;
+
+let walker max_paths max_ref aai best_progress =
   let max_y = pred @@ Array.length aai in
   let max_x = pred @@ Array.length aai.(0) in
-  let best_route = ref 125 in
-  let rec aux x y direction hl sc lc rc =
-    if x < 0 || x > max_x || y < 0 || y > max_y
+  let best_route = ref max_ref in
+  let rec aux x y direction heat sc =
+    let dist = calc_distance (x, y) (max_x, max_y) in
+    let bp = best_progress.(dist) in
+    let bp_length = List.length bp in
+    if x < 0
+       || x > max_x
+       || y < 0
+       || y > max_y
+       || (bp_length = max_paths && heat >= List.hd bp)
     then ()
     else (
+      let bp = if bp_length = max_paths then List.tl bp else bp in
+      (match bp with
+       | [] -> Array.set best_progress dist [ heat ]
+       | bp -> Array.set best_progress dist (update_ordered_list heat bp []));
       match x, y with
       | x, y when x = max_x && y = max_y ->
-        if hl + aai.(y).(x) < !best_route then best_route := hl + aai.(y).(x) else ()
-      | _ when hl >= !best_route -> ()
+        if heat + aai.(y).(x) - aai.(0).(0) < !best_route
+        then best_route := heat + aai.(y).(x) - aai.(0).(0)
+        else ()
+      | _ when heat >= !best_route -> ()
       | x, y ->
-        if sc < 4
+        if sc < 2
         then (
           let new_x, new_y, new_dir = get_directions x y direction Straight in
-          aux new_x new_y new_dir (hl + aai.(y).(x)) (sc + 1) 0 0);
-        if lc < 3
-        then (
-          let new_x, new_y, new_dir = get_directions x y direction Left in
-          aux new_x new_y new_dir (hl + aai.(y).(x)) 0 (lc + 1) 0);
-        if rc < 3
-        then (
-          let new_x, new_y, new_dir = get_directions x y direction Left in
-          aux new_x new_y new_dir (hl + aai.(y).(x)) 0 0 (rc + 1));
-        ())
+          aux new_x new_y new_dir (heat + aai.(y).(x)) (sc + 1));
+        let new_x, new_y, new_dir = get_directions x y direction Left in
+        aux new_x new_y new_dir (heat + aai.(y).(x)) 0;
+        let new_x, new_y, new_dir = get_directions x y direction Right in
+        aux new_x new_y new_dir (heat + aai.(y).(x)) 0)
   in
-  aux 0 0 E 0 0 0 0;
+  aux 0 0 S 0 0;
   !best_route
 ;;
 
 let input =
-  read_lines "inputs/17_t.txt"
+  read_lines "inputs/17.txt"
   |> remove_empty_string
   |> List.map String.explode
   |> List.map (List.map (String.make 1))
@@ -85,8 +107,13 @@ let city = Array.make max_y [||]
 let () = city_map input city
 let () = print_newline ()
 let () = print_newline ()
-let () = print_aai city
-let output = walker city
+
+(* let () = print_aai city *)
+let max_y = pred @@ Array.length city
+let max_x = pred @@ Array.length city.(0)
+let best_progress = Array.make (max_y + max_x + 2) []
+let max_paths = List.init 7 (fun x -> Int.pow (x + 1) 4)
+let output = List.fold_left (fun acc x -> walker x acc city best_progress) 2000 max_paths
 let () = print_newline ()
 let () = print_newline ()
 
