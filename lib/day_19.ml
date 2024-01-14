1833943908e5211d627c5dc7b310e0de64dfba3d
masterteapot <71963205+masterteapot@users.noreply.github.com>
2024-01-14 20:58:26 +0400

    Some more day 19

--- lib/day_19.ml
+++ lib/day_19.ml
@@ -22,6 +22,12 @@ type rating =
   ; accepted : bool
   }
 
+type operation =
+  { left : rating_cat
+  ; operand : int -> int -> bool
+  ; right : int
+  }
+
 type workflow =
   { name : string
   ; check : rating -> (string, rating) Base.Hashtbl.t -> rating
@@ -50,26 +56,26 @@ let parse_input ls =
 let input = read_lines "inputs/19_t.txt"
 let workflows_raw, ratings_raw = parse_input input
 
-let get_num = function
+let is_num = function
   | '0' .. '9' -> true
   | _ -> false
 ;;
 
-let get_str = function
+let is_str = function
   | 'a' .. 'z' -> true
   | _ -> false
 ;;
 
 let comma = char ','
 let equality = char '='
-let integer = take_while1 get_num >>| fun s -> Stdlib.int_of_string s
-let get_x = char '{' *> char 'x' *> equality *> integer <* comma >>| fun x -> x
-let get_m = char 'm' *> equality *> integer <* comma >>| fun x -> x
-let get_a = char 'a' *> equality *> integer <* comma >>| fun x -> x
-let get_s = char 's' *> equality *> integer <* char '}'
-let get_keys = take_while1 get_str <* char '{' >>| fun x -> x
+let parse_int = take_while1 is_num >>| fun s -> Stdlib.int_of_string s
+let parse_x = char '{' *> char 'x' *> equality *> parse_int <* comma >>| fun x -> x
+let parse_m = char 'm' *> equality *> parse_int <* comma >>| fun x -> x
+let parse_a = char 'a' *> equality *> parse_int <* comma >>| fun x -> x
+let parse_s = char 's' *> equality *> parse_int <* char '}'
+let parse_keys = take_while1 is_str <* char '{' >>| fun x -> x
 
-let sign =
+let parse_operator =
   peek_char
   >>= function
   | Some '>' -> advance 1 >>| fun () -> ">"
@@ -77,7 +83,7 @@ let sign =
   | _ -> fail "Sign or digit expected"
 ;;
 
-let is_rate = function
+let is_rating = function
   | 'a' | 'm' | 's' | 'x' -> true
   | _ -> false
 ;;
@@ -87,20 +93,32 @@ let is_result = function
   | _ -> false
 ;;
 
-let get_result = char ':' *> take_while1 is_result <* char ','
->>| function
-| "A" -> Accept
-| "R" -> Reject
-| x -> Next x
+let parse_result =
+  char ':' *> take_while1 is_result
+  <* char ','
+  >>| function
+  | "A" -> Accept
+  | "R" -> Reject
+  | x -> Next x
+;;
+
+let parse_end_result =
+  take_while1 is_result
+  <* char '}'
+  >>| function
+  | "A" -> Accept
+  | "R" -> Reject
+  | x -> Next x
+;;
 
 let parse_ratings =
-  get_x
+  parse_x
   >>= fun x ->
-  get_m
+  parse_m
   >>= fun m ->
-  get_a
+  parse_a
   >>= fun a ->
-  get_s
+  parse_s
   >>| function
   | s -> { x; m; a; s; accepted = false }
 ;;
@@ -111,18 +129,29 @@ let string_of_result = function
   | Next x -> x
 ;;
 
-let parse_workflows =
-  get_keys
-  >>= fun k ->
-  take_while1 is_rate
-  >>= fun v ->
-  sign
-  >>= fun s ->
-  integer
-  >>= fun i ->
-  get_result
+let parse_operations =
+  take_while1 is_rating
+  >>= fun left_of_operation ->
+  parse_operator
+  >>= fun operator ->
+  parse_int
+  >>= fun right_of_operation ->
+  parse_result
   >>| function
-  | r -> k ^ " >>> " ^ v ^ s ^ Stdlib.string_of_int i ^ " -> " ^ string_of_result r
+  | result -> left_of_operation, operator, right_of_operation, result
+;;
+
+let parse_workflows =
+  parse_keys
+  >>= fun key ->
+  parse_operations
+  >>| function
+  | operation_1 ->
+    key
+    ^ " >>> "
+    ^
+      (match operation_1 with
+      | lo, o, ro, re -> lo ^ o ^ Stdlib.string_of_int ro ^ " -> " ^ string_of_result re)
 ;;
 
 let get_results = function
